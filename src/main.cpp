b92b86c4f825255ff45cce42dbcab20d5b149979
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-04-15 09:17:13 +0200

    Add Window
    - Initialize GLFW
    - Create Window
    - Initialize GLAD
    - Render loop
    - Terminate GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	// GLFW Initialization
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	// Window Creation
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLSetup", NULL, NULL);
+	
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	// GLAD initialization
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	// GLFW Termination
+	glfwTerminate();
 	return 0;
 }
