0d6503694548f00cbc4d761632cfab01e379baf0
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-04-22 19:49:08 +0200

    Create moon shader file
    - Corrects moon colour from red to grayscale without using deprecated OpenGL technique (GL_LUNIMANCE is deprecated)

---
+++ shaders/moon.fs
@@ -0,0 +1,11 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 fragTextureCoord;
+
+uniform sampler2D textureSampler;
+
+void main()
+{
+	FragColor = texture(textureSampler, fragTextureCoord).rrra;
+}
