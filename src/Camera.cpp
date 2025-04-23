73b42aaf3a1579a8bb758f4c9b6a047f975d5ff1
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-04-23 14:12:25 +0200

    Create abstract Camera class
    - Parent of all game cameras

---
+++ src/Camera.cpp
@@ -0,0 +1,42 @@
+#include "Camera.hpp"
+
+#include <iostream>
+
+Camera::Camera(glm::vec3* pPosition, float pMinPitch, float pMaxPitch) : position(pPosition), minPitch(glm::max(pMinPitch, MIN_PITCH)), maxPitch(glm::min(pMaxPitch, MAX_PITCH))
+{
+	if (minPitch > maxPitch)
+	{
+		std::cout << "Minimum pitch is greater than maximum pitch (" << minPitch << " > " << maxPitch << ")\n";
+		std::cout << "Undefined camera behaviour will occur" << std::endl;
+	}
+	
+}
+
+Camera::~Camera()
+{
+	if (position != nullptr)
+	{
+		delete position;
+		position = nullptr;
+	}
+	if (target != nullptr)
+	{
+		delete target;
+		target = nullptr;
+	}
+}
+
+void Camera::SetPosition(glm::vec3* pPosition)
+{
+	position = pPosition;
+}
+
+void Camera::SetTarget(glm::vec3* pTarget)
+{
+	target = pTarget;
+}
+
+glm::mat4 Camera::GetViewMatrix() const
+{
+	return glm::lookAt(*position, *target, initialUp);
+}
---
+++ src/Camera.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+static const float MIN_PITCH = -89.0f, MAX_PITCH = 89.0f;
+
+class Camera
+{
+protected:
+	glm::vec3* position;
+	glm::vec3* target;
+	glm::vec3 initialUp;
+	glm::vec3 right;
+	glm::vec3 up;
+	
+	glm::mat4 viewMatrix;
+	
+	float pitch;
+	float yaw;
+	const float minPitch, maxPitch;
+public:
+	Camera(glm::vec3* pPosition, float pMinPitch, float pMaxPitch);
+	virtual ~Camera();
+	
+	void SetPosition(glm::vec3* pPosition);
+	void SetTarget(glm::vec3* pTarget);
+	virtual void AddDirectionOffest(float xOffset, float yOffset) = 0;
+	
+	glm::mat4 GetViewMatrix() const;
+};
