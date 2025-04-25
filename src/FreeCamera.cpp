59f5e61a1874228dab8dd34aca5878584779c4ac
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-04-25 08:26:33 +0200

    Change pitch and yaw and angles constraint
    - Constrain to -360 to 360
    - Fixed constrain, was supposed to be -180 to 180 but was actually -540 to 180, and caused 180 to become 0, which is a logical error

--- src/FreeCamera.cpp
+++ src/FreeCamera.cpp
@@ -13,8 +13,8 @@ FreeCamera::~FreeCamera()
 
 void FreeCamera::AddDirectionOffest(float xOffset, float yOffset)
 {
-	yaw = fmod(yaw + xOffset, 360.0f) - 180.0f;
-	pitch = fmod(pitch + yOffset, 360.0f) - 180.0f;
+	yaw = fmod(yaw + xOffset, 360.0f);
+	pitch = fmod(pitch + yOffset, 360.0f);
 	
 	glm::clamp(pitch, minPitch, maxPitch);
 	
