fbb1126ff074e31efc89fac724dde0d083eb98b5
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-04-27 15:39:29 +0200

    Add fixed updating in while loop
    - Using Timer class

--- src/Display.cpp
+++ src/Display.cpp
@@ -2,6 +2,9 @@
 
 #include "gLErrorHandle.hpp"
 
+const double Display::UPDATES_PER_SEC = 60.0;
+const double Display::TARGET_DELTA_TIME = 1000000.0 / Display::UPDATES_PER_SEC;
+
 Display::Display(unsigned int pWidth, unsigned int pHeight, const char* pTitle) : mWidth(pWidth), mHeight(pHeight), mTitle(pTitle), mWindow(glfwCreateWindow(mWidth, mHeight, mTitle, NULL, NULL))
 {
 	scene = nullptr;
@@ -58,16 +61,29 @@ void Display::Run()
 	scene->AddPlanet(mars);
 	
 	// Render loop
+	double deltaTime = 0.0;
+	timer.start();
 	while (!glfwWindowShouldClose(mWindow))
 	{
 		GLCall(glClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]));
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+		
+		// Fixed Update (as opposed to a deltaTime update)
+		// getElapsedTime...() automatically stops the timer so timer.stop() is not needed
+		deltaTime += timer.getElapsedTimeInMicroSec(); // Cumulative
+		if (deltaTime >= TARGET_DELTA_TIME)
+		{
+			deltaTime -= TARGET_DELTA_TIME;
+			scene->Update();
+		}
+		timer.start();
 		
 		scene->Render();
 		
 		glfwSwapBuffers(mWindow);
 		glfwPollEvents();
 	}
+	timer.stop();
 }
 
 void Display::framebuffer_size_callback(GLFWwindow* window, int width, int height)
--- src/Display.hpp
+++ src/Display.hpp
@@ -3,17 +3,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "Timer.h"
 #include "Scene.hpp"
 
 class Display
 {
 public:
+	static const double UPDATES_PER_SEC;
+	static const double TARGET_DELTA_TIME;
+	
 	unsigned int mWidth, mHeight;
 	
 	const float clearColor[4] = { 0.2f, 0.3f, 0.3f, 1.0f };
 	const char* const mTitle;
 	GLFWwindow* const mWindow;
-
+	
 	Display(unsigned int pWidth, unsigned int pHeight, const char* pTitle);
 	~Display();
 	
@@ -22,6 +26,8 @@ public:
 	void Run();
 private:
 	Scene* scene;
+	
+	Timer timer;
 	
 	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 };
